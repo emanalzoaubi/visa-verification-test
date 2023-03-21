f338b3d5c39b90e7bf82f2712c84b4e711acb4f9
emanalzoaubi <36507030+emanalzoaubi@users.noreply.github.com>
2023-03-21 10:36:33 +0300

    enhance design

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,47 +1,50 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                @if (session('success'))
-                    <div class="alert alert-success" role="alert">
-                        {{ session('success') }}
-                    </div>
-                @endif
+<div class="container">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+        <div class="col-md-8">
 
-                <div class="card">
-                    <div class="card-header">Send Welcome Email</div>
 
-                    <div class="card-body">
-                        <form method="POST" action="{{ route('send-email') }}">
-                            @csrf
+            <div class="card">
+                <div class="card-header">Send Welcome Email</div>
 
-                            <div class="form-group row">
-                                <label for="email" class="col-md-4 col-form-label text-md-right">Email</label>
+                <div class="card-body">
+                    <form method="POST" action="{{ route('send-email') }}">
+                        @csrf
 
-                                <div class="col-md-6">
-                                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                        <div class="form-group row">
 
-                                    @error('email')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                    @enderror
-                                </div>
+
+                            <div class="col-xs-12">
+                                <input placeholder="Email" id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+
+                                @error('email')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
                             </div>
+                        </div>
 
-                            <div class="form-group row mb-0">
-                                <div class="col-md-6 offset-md-4">
-                                    <button type="submit" class="btn btn-primary">
-                                        Send Email
-                                    </button>
-                                </div>
+                        <div class="form-group row mb-0">
+                            <div class="col-xs-12 mt-4">
+                                <button type="submit" class="btn btn-primary btn-block">
+                                    Send Email
+                                </button>
                             </div>
-                        </form>
-                    </div>
+                        </div>
+                    </form>
                 </div>
             </div>
+
+            @if (session('success'))
+            <div class="alert alert-success" role="alert">
+                {{ session('success') }}
+            </div>
+            @endif
+
         </div>
     </div>
+</div>
 @endsection
